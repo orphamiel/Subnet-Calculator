bf8bc069af1ecea0ade1432876d299c5dfc18884
Orphamiel <orphamiel@yahoo.com>
2013-04-21 02:29:37 +0800

    First development

---
+++ Subnet Calc/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Subnet_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bit1 = new System.Windows.Forms.TextBox();
+            this.dot = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bit2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bit3 = new System.Windows.Forms.TextBox();
+            this.bit4 = new System.Windows.Forms.TextBox();
+            this.iplabel = new System.Windows.Forms.Label();
+            this.slash = new System.Windows.Forms.Label();
+            this.subnetbit = new System.Windows.Forms.TextBox();
+            this.outputbox = new System.Windows.Forms.RichTextBox();
+            this.outputlab = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bit1
+            // 
+            this.bit1.Location = new System.Drawing.Point(86, 13);
+            this.bit1.MaxLength = 3;
+            this.bit1.Name = "bit1";
+            this.bit1.Size = new System.Drawing.Size(40, 20);
+            this.bit1.TabIndex = 1;
+            this.bit1.TextChanged += new System.EventHandler(this.bit1_TextChanged);
+            // 
+            // dot
+            // 
+            this.dot.AutoSize = true;
+            this.dot.CausesValidation = false;
+            this.dot.Enabled = false;
+            this.dot.Location = new System.Drawing.Point(128, 20);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(10, 13);
+            this.dot.TabIndex = 0;
+            this.dot.Text = ".";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.CausesValidation = false;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(182, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".";
+            // 
+            // bit2
+            // 
+            this.bit2.Location = new System.Drawing.Point(140, 13);
+            this.bit2.MaxLength = 3;
+            this.bit2.Name = "bit2";
+            this.bit2.Size = new System.Drawing.Size(40, 20);
+            this.bit2.TabIndex = 2;
+            this.bit2.TextChanged += new System.EventHandler(this.bit2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.CausesValidation = false;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(236, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = ".";
+            // 
+            // bit3
+            // 
+            this.bit3.Location = new System.Drawing.Point(194, 13);
+            this.bit3.MaxLength = 3;
+            this.bit3.Name = "bit3";
+            this.bit3.Size = new System.Drawing.Size(40, 20);
+            this.bit3.TabIndex = 3;
+            this.bit3.TextChanged += new System.EventHandler(this.bit3_TextChanged);
+            // 
+            // bit4
+            // 
+            this.bit4.Location = new System.Drawing.Point(248, 13);
+            this.bit4.MaxLength = 3;
+            this.bit4.Name = "bit4";
+            this.bit4.Size = new System.Drawing.Size(40, 20);
+            this.bit4.TabIndex = 4;
+            this.bit4.TextChanged += new System.EventHandler(this.bit4_TextChanged);
+            // 
+            // iplabel
+            // 
+            this.iplabel.AutoSize = true;
+            this.iplabel.Location = new System.Drawing.Point(12, 16);
+            this.iplabel.Name = "iplabel";
+            this.iplabel.Size = new System.Drawing.Size(67, 13);
+            this.iplabel.TabIndex = 0;
+            this.iplabel.Text = "IP Address : ";
+            // 
+            // slash
+            // 
+            this.slash.AutoSize = true;
+            this.slash.CausesValidation = false;
+            this.slash.Location = new System.Drawing.Point(292, 16);
+            this.slash.Name = "slash";
+            this.slash.Size = new System.Drawing.Size(12, 13);
+            this.slash.TabIndex = 0;
+            this.slash.Text = "/";
+            // 
+            // subnetbit
+            // 
+            this.subnetbit.Location = new System.Drawing.Point(308, 13);
+            this.subnetbit.MaxLength = 2;
+            this.subnetbit.Name = "subnetbit";
+            this.subnetbit.Size = new System.Drawing.Size(30, 20);
+            this.subnetbit.TabIndex = 5;
+            this.subnetbit.TextChanged += new System.EventHandler(this.subnetbit_TextChanged);
+            // 
+            // outputbox
+            // 
+            this.outputbox.CausesValidation = false;
+            this.outputbox.Location = new System.Drawing.Point(15, 56);
+            this.outputbox.Name = "outputbox";
+            this.outputbox.ReadOnly = true;
+            this.outputbox.Size = new System.Drawing.Size(323, 193);
+            this.outputbox.TabIndex = 0;
+            this.outputbox.TabStop = false;
+            this.outputbox.Text = "";
+            // 
+            // outputlab
+            // 
+            this.outputlab.AutoSize = true;
+            this.outputlab.Location = new System.Drawing.Point(12, 40);
+            this.outputlab.Name = "outputlab";
+            this.outputlab.Size = new System.Drawing.Size(48, 13);
+            this.outputlab.TabIndex = 0;
+            this.outputlab.Text = "Output : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 261);
+            this.Controls.Add(this.outputlab);
+            this.Controls.Add(this.outputbox);
+            this.Controls.Add(this.subnetbit);
+            this.Controls.Add(this.slash);
+            this.Controls.Add(this.iplabel);
+            this.Controls.Add(this.bit4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bit3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bit2);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.bit1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Subnet Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox bit1;
+        private System.Windows.Forms.Label dot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox bit2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox bit3;
+        private System.Windows.Forms.TextBox bit4;
+        private System.Windows.Forms.Label iplabel;
+        private System.Windows.Forms.Label slash;
+        private System.Windows.Forms.TextBox subnetbit;
+        private System.Windows.Forms.RichTextBox outputbox;
+        private System.Windows.Forms.Label outputlab;
+    }
+}
+
---
+++ Subnet Calc/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subnet_Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bit1.Focus();
+            outputbox.Text = "Welcome to Orphamiel's Subnet Calculator!\n\n" ;
+            outputbox.Text += "Enter your values above and related information will be automatically generated here.";
+            outputbox.Text += "\n\n\n\nIf you encounter any bugs, please send a report to orphamiel@yahoo.com";
+        }
+
+        private void bit1_TextChanged(object sender, EventArgs e)
+        {
+            if (bit1.Text.Length >= 3)
+            {
+                bit2.Focus();
+            }
+            int bit1int = 0;
+            try
+            {
+                bit1int = int.Parse(bit1.Text);
+            }
+            catch (System.Exception excep)
+            {
+                bit1.Text = "";
+                bit1.Select(bit1.Text.Length, 0);
+            }
+            if (bit1int >= 256)
+            {
+                bit1.Text = "255";
+                bit2.Focus();
+            }
+            calcul();
+        }
+
+        private void bit2_TextChanged(object sender, EventArgs e)
+        {
+            if (bit2.Text.Length >= 3)
+            {
+                bit3.Focus();
+            }
+            int bit2int = 0;
+            try
+            {
+                bit2int = int.Parse(bit2.Text);
+            }
+            catch (System.Exception excep)
+            {
+                bit2.Text = "";
+            }
+            if (bit2int >= 256)
+            {
+                bit2.Text = "255";
+                bit3.Focus();
+            }
+            calcul();
+        }
+
+        private void bit3_TextChanged(object sender, EventArgs e)
+        {
+            if (bit3.Text.Length >= 3)
+            {
+                bit4.Focus();
+            }
+            int bit3int = 0;
+            try
+            {
+                bit3int = int.Parse(bit3.Text);
+            }
+            catch (System.Exception excep)
+            {
+                bit3.Text = "";
+            }
+            if (bit3int >= 256)
+            {
+                bit3.Text = "255";
+                bit4.Focus();
+            }
+            calcul();
+        }
+
+        private void bit4_TextChanged(object sender, EventArgs e)
+        {
+            if (bit4.Text.Length >= 3)
+            {
+                subnetbit.Focus();
+            }
+            int bit4int = 0;
+            try
+            {
+                bit4int = int.Parse(bit4.Text);
+            }
+            catch (System.Exception excep)
+            {
+                bit4.Text = "";
+            }
+            if (bit4int >= 256)
+            {
+                bit4.Text = "255";
+                subnetbit.Select(0, 0);
+            }
+            calcul();
+        }
+
+        private void subnetbit_TextChanged(object sender, EventArgs e)
+        {
+            int subint = 0;
+            try
+            {
+                subint = int.Parse(subnetbit.Text);
+            }
+            catch (System.Exception excep)
+            {
+                subnetbit.Text = "";
+            }
+            if (subint >= 31)
+            {
+                subnetbit.Text = "31";
+                subnetbit.Select(subnetbit.Text.Length, 0);
+            }
+            calcul();
+        }
+
+        private void calcul()
+        {
+            //Start getting results from textboxes
+            int bit1int, bit2int, bit3int, bit4int, subint;
+            try
+            {
+                bit1int = int.Parse(bit1.Text);
+            }
+            catch
+            {
+                //if fail or empty make it 0
+                bit1int = 0;
+            }
+            try
+            {
+                bit2int = int.Parse(bit2.Text);
+            }
+            catch
+            {
+                bit2int = 0;
+            }
+            try
+            {
+                bit3int = int.Parse(bit3.Text);
+            }
+            catch
+            {
+                bit3int = 0;
+            }
+            try
+            {
+                bit4int = int.Parse(bit4.Text);
+            }
+            catch
+            {
+                bit4int = 0;
+            }
+            try
+            {
+                subint = int.Parse(subnetbit.Text);
+            }
+            catch
+            {
+                subint = 0;
+            }
+            // End of getting all results - Make output textbox empty first
+            outputbox.Text = "";
+            int sub1 = 0;
+            int netid = 256;
+            int track = -1;
+            int tempint = 1;
+            //Do calculations here
+            if (subint == 0)
+            {
+                bit1int = 0;
+                bit2int = 0;
+                bit3int = 0;
+                bit4int = 0;
+            }
+            else if (subint < 8)
+            {
+                for (int temp = 0; temp < subint; temp++)
+                {
+                    tempint *= 2;
+                }
+                sub1 = (256 / tempint);
+                while (bit1int < netid)
+                {
+                    netid -= sub1;
+                    track++;
+                }
+                bit1int = netid;
+                bit2int = 0;
+                bit3int = 0;
+                bit4int = 0;
+            }
+            else if (subint < 16)
+            {
+                int tempsubnet = subint - 8;
+                for (int temp = 0; temp < tempsubnet; temp++)
+                {
+                    tempint *= 2;
+                }
+                sub1 = (256 / tempint);
+                while (bit2int < netid)
+                {
+                    netid -= sub1;
+                    track++;
+                }
+                bit2int = netid;
+                bit3int = 0;
+                bit4int = 0;
+            }
+            else if (subint < 24)
+            {
+                int tempsubnet = subint - 16;
+                for (int temp = 0; temp < tempsubnet; temp++)
+                {
+                    tempint *= 2;
+                }
+                sub1 = (256 / tempint);
+                while (bit3int < netid)
+                {
+                    netid -= sub1;
+                    track++;
+                }
+                bit3int = netid;
+                bit4int = 0;
+            }
+            else if (subint < 32)
+            {
+                int tempsubnet = subint - 24;
+                for (int temp = 0; temp < tempsubnet; temp++)
+                {
+                    tempint *= 2;
+                }
+                sub1 = (256 / tempint);
+                while (bit4int < netid)
+                {
+                    netid -= sub1;
+                    track++;
+                }
+                bit4int = netid;
+            }
+            else
+            {
+                //error catching
+                bit1int = 999;
+                bit2int = 999;
+                bit3int = 999;
+                bit4int = 999;
+            }
+            //Output information from calculations
+            outputbox.Text = "Network Address is : " + bit1int + "." + bit2int + ".";
+            outputbox.Text += "" + bit3int + "." + bit4int + "\n";
+            if (subint == 0)
+            {
+                outputbox.Text += "1st Host Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + ".1" + "\n";
+                outputbox.Text += "Broadcast Address is : 255.255.255.255";
+                outputbox.Text += "\n \nSubnet Mask is : 0.0.0.0";
+                outputbox.Text += "\nWildcard Mask is : 255.255.255.255";
+                outputbox.Text += "\n \n" + "Total hosts avaliable : 4294967294";
+            }
+            else if (subint < 8)
+            {
+                //something is wrong here. the values are always wrong
+                outputbox.Text += "1st Host Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + ".1" + "\n";
+                int bit1temp = 256-(track*sub1)-1;
+                outputbox.Text += "Broadcast Address is : " + bit1temp ;
+                outputbox.Text += ".255.255.255";
+                int wildint = sub1 - 1;
+                int bit1invert = 256 - wildint - 1;
+                outputbox.Text += "\n \nSubnet Mask is : " + bit1invert + ".0.0.0";
+                outputbox.Text += "\nWildcard Mask is : "+ wildint +".255.255.255";
+                sub1 *= 256 * 256 *256;
+                sub1 = sub1 - 2;
+                outputbox.Text += "\n \n" + "Total hosts avaliable : " + sub1;
+            }
+            else if (subint < 16)
+            {
+                outputbox.Text += "1st Host Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + ".1" + "\n";
+                int bit2temp = 256 - (track * sub1) - 1;
+                outputbox.Text += "Broadcast Address is : " + bit1int + ".";
+                outputbox.Text += bit2temp + ".255.255";
+                int wildint = sub1 - 1;
+                int bit2invert = 256 - wildint - 1;
+                outputbox.Text += "\n \nSubnet Mask is : 255." + bit2invert + ".0.0";
+                outputbox.Text += "\nWildcard Mask is : 0." + wildint + ".255.255";
+                sub1 *= 256 * 256;
+                sub1 = sub1 - 2;
+                outputbox.Text += "\n \n" + "Total hosts avaliable : " + sub1;
+            }
+            else if (subint < 24)
+            {
+                outputbox.Text += "1st Host Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + ".1" + "\n";
+                int bit3temp = 256 - (track * sub1) - 1;
+                outputbox.Text += "Broadcast Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3temp + ".255";
+                int wildint = sub1 - 1;
+                int bit3invert = 256 - wildint - 1;
+                outputbox.Text += "\n \nSubnet Mask is : 255.255." + bit3invert + ".0";
+                outputbox.Text += "\nWildcard Mask is : 0.0." + wildint + ".255";
+                sub1 *= 256;
+                sub1 = sub1 - 2;
+                outputbox.Text += "\n\n" + "Total hosts avaliable : " + sub1;
+            }
+            else if (subint < 32)
+            {
+                bit4int++;
+                outputbox.Text += "1st Host Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + "." + bit4int + "\n";
+                int bit4temp = 256 - (track * sub1) - 1;
+                outputbox.Text += "Broadcast Address is : " + bit1int + "." + bit2int;
+                outputbox.Text += "." + bit3int + "." + bit4temp;
+                int wildint = sub1 - 1;
+                int bit4invert = 256 - wildint - 1;
+                outputbox.Text += "\n\nSubnet Mask is : 255.255.255." + bit4invert;                
+                outputbox.Text += "\nWildcard Mask is : 0.0.0." + wildint;
+                sub1 = sub1 - 2;
+                outputbox.Text += "\n\n" + "Total hosts avaliable : " + sub1;
+            }
+            else
+            {
+                outputbox.Text = "Error encounered!";
+            }
+
+        }
+    }
+}
